7e0962211d21f5e3d8a4e575da879422a24db7aa
laiba-shah <121715553+laiba-shah@users.noreply.github.com>
2024-04-08 14:31:51 +0500

    adding more location features

--- app/lib/screens/home/mapScreen.dart
+++ app/lib/screens/home/mapScreen.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'dart:html';
+
 import 'package:app/widgets/appTheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -11,8 +15,20 @@ class MapScreen extends StatefulWidget {
 }
 
 class __MapScreenState extends State<MapScreen> {
+  Location _locationController = new Location();
+
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+
   static const LatLng karachi = LatLng(25.10720314676199, 67.27599663525518);
   static const LatLng trial = LatLng(37.4223, -122.0848);
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +39,64 @@ class __MapScreenState extends State<MapScreen> {
           title: const Text("Map Screen"),
           titleTextStyle: const TextStyle(color: AppTheme.periwinkleColor),
         ),
-        body: GoogleMap(
-          initialCameraPosition: CameraPosition(target: karachi, zoom: 13),
-          markers: {
-            Marker(markerId: MarkerId("_currentLocation"), icon: BitmapDescriptor.defaultMarker, position: karachi)
-          },
-        )
-        );
+        body: _currentP == null
+            ? const Center(child: Text("Loading..."))
+            : GoogleMap(
+                onMapCreated: ((GoogleMapController controller) =>
+                    _mapController.complete(controller)),
+                initialCameraPosition:
+                    CameraPosition(target: karachi, zoom: 13),
+                markers: {
+                  Marker(
+                      markerId: MarkerId("_currentLocation"),
+                      icon: BitmapDescriptor.defaultMarker,
+                      position: _currentP!),
+                  // Marker(
+                  //     markerId: MarkerId("sourceLocation"),
+                  //     icon: BitmapDescriptor.defaultMarker,
+                  //     position: trial),
+                  const Marker(
+                      markerId: MarkerId("destinationLocation"),
+                      icon: BitmapDescriptor.defaultMarker,
+                      position: karachi)
+                },
+              ));
+  }
+
+  Future<void> _cameraToPos(LatLng pos) async{
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition _newCameraPosition = CameraPosition(target: pos, zoom: 13);
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        });
+        print(_currentP);
+      }
+    });
   }
 }
